5bb8fb1cc20c007ddbf104bdd00b57672bcf56c9
Adena7 <abdoullahi.aden@gmail.com>
2022-02-16 00:18:56 -0700

    Start header and home components
    Co-authored-by: OsmanShakib <OsmanShakib@users.noreply.github.com>
    Co-authored-by: Najib Farah <NajibFarah@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Home from './Home'
+import Header from './Header';
 
 function App() {
   return (
-    <div>
-      
+    <div classname="App">
+      <h1>Let's build AIRBNB clone</h1>
+    
+     <Home />
+
+  {/* Home */}   
+      {/* Header */}
+
+      {/* Banner */}
+          {/* Search */}
+      {/* Cards */}
+
+      {/* Footer */}
+
+  {/* SearchPage */}
+     {/* ... */}  
+
     </div>
   );
 }
