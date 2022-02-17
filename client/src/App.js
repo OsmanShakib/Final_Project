2f559ba33a485660850b1111e7f04259447cc9c5
Adena7 <abdoullahi.aden@gmail.com>
2022-02-16 17:24:21 -0700

    setup db and pass db into routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import Home from './Home'
-import Header from './Header';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div classname="App">
-      <h1>Let's build AIRBNB clone</h1>
-    
-     <Home />
-
-  {/* Home */}   
-      {/* Header */}
-
-      {/* Banner */}
-          {/* Search */}
-      {/* Cards */}
-
-      {/* Footer */}
-
-  {/* SearchPage */}
-     {/* ... */}  
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
