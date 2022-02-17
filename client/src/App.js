7dad997c495b01bb592df6613bb44fb18e4edcf2
Najib <muhammed.najib.farah@gmail.com>
2022-02-17 22:31:18 +0000

    summary

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-function App() {  
+import SummaryCard from "./components/SummaryCard";
+function App() {
   return (
-  
-     <div> 
-       <Header />
-  </div>
-
+    <div>
+      <Header />
+    </div>
   );
 }
 
