39fdaa8c324c01d69b892521864f16c9983a96f7
Adena7 <abdoullahi.aden@gmail.com>
2022-02-17 17:21:47 -0700

    Refractoring Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
+import Header from "./components/Header";
 function App() {  
-  const API_KEY = process.env.REACT_APP_API_KEY
-
-  // const [noData, setNoData] = useState('No Data Yet')
-  // const [city, setCity] = useState('Unknown location')
-
   return (
-  <div className="bg-gray-800 flex items-center w-screen h-screen py-10">
-   <div className="flex w-3/4 min-h-full rounded-3x1 shadow-lg m-auto bg-gray-100">
-     </div> 
+  
+     <div> 
+       <Header />
   </div>
 
   );
