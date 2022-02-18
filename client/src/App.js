fe408e0dab1ec880a33a9d6d955ce36f2d74f97a
Adena7 <abdoullahi.aden@gmail.com>
2022-02-17 17:21:57 -0700

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Header";
-function App() {  
-  return (
-  
-     <div> 
-       <Header />
-  </div>
 
-  );
-}
+  export default function App() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+    
 
-export default App;
