d078faaa7f3d4479fe24c20013a8367d0d55ba57
Adena7 <abdoullahi.aden@gmail.com>
2022-02-22 16:39:12 -0700

    implement Appollo-express server and resolvers
    Co-authored-by: OsmanShakib <OsmanShakib@users.noreply.github.com>
    Co-authored-by: Najib Farah <NajibFarah@users.noreply.github.com>

---
+++ server/Resolvers/main/index.js
@@ -0,0 +1,14 @@
+const {GraphQLDateTime} = require("graphql-iso-date");
+
+//Resolvers
+const userResolvers = require("../userResolver");
+
+//Custom Resolvers
+const customResolvers = {
+  Date: GraphQLDateTime
+}
+
+module.exports = [
+  customResolvers,
+  userResolvers
+]
